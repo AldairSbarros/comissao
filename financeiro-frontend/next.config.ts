9f0dd7acb0b18088510e1a4137500d482d57bbcb
Aldair Barros <aldairbarros@outlook.com>
2026-09-15 19:30:30 -0400

    Remove regra de rewrite do next.config.txt

--- financeiro-frontend/next.config.ts
+++ financeiro-frontend/next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   basePath: "/financeiro",
-  async rewrites() {
-    return [
-      {
-        source: "/financeiro/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
-
